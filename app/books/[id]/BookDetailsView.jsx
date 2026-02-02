eb047b5d301867ad344feb223691a71eafec787b
Zyad Mohamed Ahmed <zm857891@gmail.com>
2026-02-03 00:04:55 +0200

    extra lines removed

--- app/books/[id]/BookDetailsView.jsx
+++ app/books/[id]/BookDetailsView.jsx
@@ -1,9 +1,9 @@
-import classes from './page.module.css'
-import { FaStar } from 'react-icons/fa';
-import PrimaryButton from '../../../UI/PrimaryButton';
-import Link from 'next/link';
-import Notes from '../../../components/Notes';
-import BookFavourite from './BookFavourite';
+import classes from "./page.module.css";
+import { FaStar } from "react-icons/fa";
+import PrimaryButton from "../../../UI/PrimaryButton";
+import Link from "next/link";
+import Notes from "../../../components/Notes";
+import BookFavourite from "./BookFavourite";
 
 export default function BookDetailsView({ data, id }) {
     const bookData = data?.volumeInfo;
@@ -23,87 +23,74 @@ export default function BookDetailsView({ data, id }) {
 
     return (
         <>
-            {/* {isError && <h1>{error.message}</h1>} */}
-
-            <>
-                <div className={classes.content}>
-                    <div className={classes["book-info"]}>
-                        <h1>{bookData.title}</h1>
-                        <div className={classes.author}>
-                            by{" "}
-                            {bookData.authors?.map((author, i) => (
-                                <span className="mx-1" key={i}>
-                                    {author}
-                                </span>
-                            ))}{" "}
-                        </div>
-                        <div className={classes.rating}>
-                            {bookData.averageRating && (
-                                <>
-                                    {Array.from({
-                                        length: Math.floor(
-                                            bookData.averageRating
-                                        ),
-                                    }).map((_, i) => (
-                                        <FaStar
-                                            color="yellow"
-                                            key={i}
-                                            size={40}
-                                        />
-                                    ))}
-                                    <span className="text-4xl">{` ${bookData.averageRating.toFixed(
-                                        1
-                                    )}`}</span>
-                                </>
-                            )}
-                        </div>
-                        <div className={classes.category}>
-                            {bookData.categories?.map((c) => `${c} `)}
-                        </div>
-                        <hr />
-                        <div className={classes.type}>Paperback</div>
-                        <div className="flex justify-between flex-wrap items-center">
-                            <div className={classes.price}>{bookPrice}</div>
-                            <BookFavourite id={id}/>
-                        </div>
-
-                        <hr />
-                        <p className={classes.description}>
-                            {bookData.description ? (
-                                <span
-                                    dangerouslySetInnerHTML={{
-                                        __html: bookData.description,
-                                    }}
-                                />
-                            ) : (
-                                "No description available"
-                            )}
-                        </p>
+            <div className={classes.content}>
+                <div className={classes["book-info"]}>
+                    <h1>{bookData.title}</h1>
+                    <div className={classes.author}>
+                        by{" "}
+                        {bookData.authors?.map((author, i) => (
+                            <span className="mx-1" key={i}>
+                                {author}
+                            </span>
+                        ))}{" "}
+                    </div>
+                    <div className={classes.rating}>
+                        {bookData.averageRating && (
+                            <>
+                                {Array.from({
+                                    length: Math.floor(bookData.averageRating),
+                                }).map((_, i) => (
+                                    <FaStar color="yellow" key={i} size={40} />
+                                ))}
+                                <span className="text-4xl">{` ${bookData.averageRating.toFixed(
+                                    1
+                                )}`}</span>
+                            </>
+                        )}
+                    </div>
+                    <div className={classes.category}>
+                        {bookData.categories?.map((c) => `${c} `)}
+                    </div>
+                    <hr />
+                    <div className={classes.type}>Paperback</div>
+                    <div className="flex justify-between flex-wrap items-center">
+                        <div className={classes.price}>{bookPrice}</div>
+                        <BookFavourite id={id} />
                     </div>
 
-                    <div className={classes["book-image"]}>
-                        <img
-                            src={thumbnail}
-                            alt={bookData.title}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        />
-                        <Link
-                            href={
-                                bookData.canonicalVolumeLink ||
-                                bookData.infoLink
-                            }
-                            target="_blank"
-                            className="mt-3"
-                        >
-                            <PrimaryButton>Available</PrimaryButton>
-                        </Link>
-                    </div>
+                    <hr />
+                    <p className={classes.description}>
+                        {bookData.description ? (
+                            <span
+                                dangerouslySetInnerHTML={{
+                                    __html: bookData.description,
+                                }}
+                            />
+                        ) : (
+                            "No description available"
+                        )}
+                    </p>
                 </div>
-                <div className="max-w-[1200px] mx-auto mt-3">
-                    <Notes bookId={id} />
+
+                <div className={classes["book-image"]}>
+                    <img
+                        src={thumbnail}
+                        alt={bookData.title}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    />
+                    <Link
+                        href={bookData.canonicalVolumeLink || bookData.infoLink}
+                        target="_blank"
+                        className="mt-3"
+                    >
+                        <PrimaryButton>Available</PrimaryButton>
+                    </Link>
                 </div>
-            </>
+            </div>
+            <div className="max-w-[1200px] mx-auto mt-3">
+                <Notes bookId={id} />
+            </div>
         </>
     );
 }
